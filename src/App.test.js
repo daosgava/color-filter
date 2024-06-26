d030072f958bb539cdf667f0344263d8dbc4876b
daosgava <daosgava.garcia@gmail.com>
2024-06-26 22:08:58 +1000

    Add a few tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Filter Section title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const filterTitle = screen.getByText(/Filter/i);
+  expect(filterTitle).toBeInTheDocument();
+});
+
+test('renders Data Loading Section title', () => {
+  render(<App />);
+  const filterTitle = screen.getByText(/Data Loading Section/i);
+  expect(filterTitle).toBeInTheDocument();
+});
+
+test('renders Data Display Section title', () => {
+  render(<App />);
+  const filterTitle = screen.getByText(/Data Display Section/i);
+  expect(filterTitle).toBeInTheDocument();
 });
